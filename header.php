2aec9c141b3ddd070e5700c81d07123db4d3d69e
Nelka <frankcle16@gmail.com>
2015-01-05 19:23:08 +0100

    comm

---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Portfolio</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+       
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+		
+	<div class="row affix-row">
+		<div class="col-sm-3 col-md-2 affix-sidebar">
+			<div class="sidebar-nav">
+				<div class="navbar navbar-default" role="navigation">
+					<div class="navbar-header">
+					  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+					  <span class="sr-only">Toggle navigation</span>
+					  <span class="icon-bar"></span>
+					  <span class="icon-bar"></span>
+					  <span class="icon-bar"></span>
+					  </button>
+					  <span class="visible-xs navbar-brand">Menu slide</span>
+					</div>
+    <div class="navbar-collapse collapse sidebar-navbar-collapse">
+      <ul class="nav navbar-nav" id="sidenav01">
+        <li class="active">
+          <a href="#" data-toggle="collapse" data-target="#toggleDemo0" data-parent="#sidenav01" class="collapsed">
+			<img src="img/logo.png" class="logo"/>
+          </a>
+          
+        </li>
+		 <li><a href="#"><span class="glyphicon glyphicon-home"></span> Index</a></li>
+		<li class="active">
+          <a href="#" data-toggle="collapse" data-target="#toggleDemo" data-parent="#sidenav01" class="collapsed">
+          <span class="glyphicon glyphicon-inbox"></span> Première Année <span class="caret pull-right"></span>
+          </a>
+          <div class="collapse" id="toggleDemo" style="height: 0px;">
+            <ul class="nav nav-list">
+              <li><a href="#"><span class="glyphicon glyphicon-plus"></span> Stage - CHU Bordeaux</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="active">
+          <a href="#" data-toggle="collapse" data-target="#toggleDemo2" data-parent="#sidenav01" class="collapsed">
+          <span class="glyphicon glyphicon-inbox"></span>  Deuxième Année <span class="caret pull-right"></span>
+          </a>
+          <div class="collapse" id="toggleDemo2" style="height: 0px;">
+            <ul class="nav nav-list">
+              <li><a href="#"><span class="glyphicon glyphicon-cloud"></span>  PPE - FreeFly</a></li>
+              <li><a href="#"><span class="glyphicon glyphicon-glass"></span>  Stage - Kekette</a></li>
+            </ul>
+          </div>
+        </li>
+       
+        <li><a href="#"><span class="glyphicon glyphicon-tasks"></span> Tableau </a></li>
+        <li><a href=""><span class="glyphicon glyphicon-user"></span> Contact</a></li>
+      </ul>
+			</div><!--/.nav-collapse -->
+		</div>
+		</div>
+	</div>
--- index.php
+++ index.php
@@ -1,137 +1,35 @@
-<?php
+ï»¿<?php
 include"scripts/connexion.php";
 session_start();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet"  href="style/style.css" type="text/css" />
-	<title>Accueil</title>
-	<script type="text/javascript" src="scr/jquery.js"></script>
-	<script type="text/javascript" src="scr/sliding_effect.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/style.css" />
-	<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" /> 
 
-</head>
-<body>
-	
-	
-
-	<section id="colone">
-	<div id="logo">
-		<a href="#overlay" ><img src="style/images/logo.png" style="margin-left:12px;width:100%; "/></a>
-		<div id="overlay">
-		<form class="connexion" action="suivi.php" method="post">
-			<legend>Connexion</legend>
-			<p>
-				<label>Login</label for="username"> : <input id="login" type="text" name="login" /><br/>
-				<label>Mots de passe</label for="password"> : <input id="pass" type="password" name="pass"/>
+<?php 
+	include"header.php";
+?>
+<div class="col-sm-9 col-md-10 affix-content">
+		<div class="container">
+			
+				<div class="page-header">
+					<h3><span class="glyphicon glyphicon-bookmark"></span> PrÃ©sentation</h3>
+				</div>
+			<p class="cadre" >Bonjour, Mon nom est Frank Clemenceau, </br>
+			je suis Ã©lÃ¨ve en SIO 2em annÃ©e et Nelka est mon portfolio</br>
 			</p>
-			<input type="submit" name="submit" value="Connection" action="" />
-		
-		<?php connexion();
-		?>
-		
-		</form>
 		</div>
-		<style type="text/css">
-			#logo{
-				margin-left:-10%;
-			}
-		</style>
-	</div>
-	
-	<?phpsession();?>
-	
-	<img src="style/images/barre.png" style="margin-top:5%;"/>
-	
-	<div id="choix">
-		<ul id="sliding-navigation">
-			<li class="sliding-element"><a href="index.php">Index</a></li>
-			<li class="sliding-element"><a href="tableau.html">Tableau</a></li>
-			<li class="sliding-element"><a href="work.html">Work</a></li>
-				<ul class="sliding-element">
-					<li class="sliding-element"><a href="#">1er annÃ©e</a></li>
-						<ul>
-							<li class="sliding-element"><a href="stage1">CHUBordeaux(stage)</a></li>
-						</ul>
-					<li class="sliding-element"><a href="#">2em annÃ©e</a></li>
-						<ul>
-							<li class="sliding-element"><a href="stage2">WebHera(stage)</a></li>
-							<li class="sliding-element"><a href="projet1">FreeFly(projet)</a></li>
-						</ul>
-				</ul>
-			<li class="sliding-element"><a href="contact.html">Contact</a></li>
-		</ul>
-	</div>
-	</section>
-	<style type="text/css">
-		#colone{
-			position:fixed;
-			width : 208px;
-			height:100%;
-			color: white;
-			z-index:8;
-		}
-		#choix {
-			margin-top:20%;
-			font-size:8px;
-		}
-	
-		
-	</style>
+		<section id="presentation">
+			<p>
+			J'ai 20ans et je suis nÃ©e 25 mars 1994 Nancy en France.</br>
+			</br>
+			Je suis Ã©lÃ¨ve en SIO(Systeme Informatique Organisation), j'essaye d'Ãªtre auto didacte,</br>
+			j'aime croire, concervoir et animer. </br>
+			</br>
+			Mes domaines d'expertise se situe vers le web et le graphisme.</br>
+			J'aime se que je fais bien que je m'arrache les cheveux en essayant de m'ameliorer.
+			</p>
+		</section>
+</div>
 
-	<section id="cadre" class="cadre">
-		<p class="cadre" >Bonjour, Mon nom est Frank Clemenceau, </br>
-		je suis élève en SIO 2em année et Nelka est mon portfolio</br>
-		</p>
-	</section>
-	<style type="text/css">
-		#cadre{
-			background-image:url(style/images/cadre.jpg);
-			padding-top:6%;
-			padding-left:20%;
-			font-size:40px;
-			text-align:center;
-			border:1px solid #1337F8;
-			width:81.8%;
-			color:white;
-			box-shadow: 0px 48px 100px 100px rgba(2, 47, 213, 0.68);
-			-moz-box-shadow: 0px 48px 100px 100px rgba(2, 47, 213, 0.68);
-			-webkit-box-shadow: 0px 48px 100px 100px rgba(2, 47, 213, 0.68);
-		}
-	</style>
-	
-	<style type="text/css">
-	#presentation{
-		font-size:20px;
-		color:white;
-		text-align:center;
-		padding-top:7%;
-		margin-left:20%;
-	}
-	</style>
-	<section id="presentation">
-		<p>
-		J'ai 20ans et je suis né le 25 mars 1994 à Nancy en France.</br>
-		</br>
-		Je suis élève en SIO(Systeme Informatique Organisation), j'essaye d'être auto didacte,</br>
-		j'aime créer, concervoir et animer. </br>
-		</br>
-		Mes domaines d'expertise se situe vers le web et le graphisme.</br>
-		J'aime se que je fais bien que je m'arrache les cheveux en essayant de m'ameliorer.
-		</p>
-	</section>
-	
-	<?php
-	if (isset($_SESSION['login'])AND  $_SESSION['login']!= ''){
-			print ("<p style=\"color:red; margin-left:50%;\">ConnectÃ©</p>");
-	}
+<?php 
+	include"footer.php";
+?>
 
-	?>
-	
-	
-	
-	
-</body>
-</html>
